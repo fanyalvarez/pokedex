eb064a4b8c871701e315a2fb50b438b17ef3b020
Fanny <alvarezpestefania@outlook.com>
2022-12-01 22:47:26 -0600

    integrate Api

--- script/script.js
+++ script/script.js
@@ -2,7 +2,12 @@ const search = document.querySelector("#search");
 const number = document.querySelector("#number");
 const img = document.querySelector("#poke-img");
 const types = document.querySelector("#types");
-const type = document.querySelectorAll("#type");
+const statNumber = document.querySelectorAll(".stat-number");
+const barInner = document.querySelectorAll(".inner");
+const barOuter = document.querySelectorAll(".outer");
+const statDesc = document.querySelectorAll(".stat-desc");
+const baseStats = document.querySelector("#base-stats");
+const pokedex = document.querySelector('.pokedex')
 
 const typeColors = {
   rock: [182, 158, 49],
@@ -21,7 +26,7 @@ const typeColors = {
   ground: [222, 193, 107],
   bug: [167, 183, 35],
   fire: [245, 125, 49],
-  electric: [249, 207, 48],
+  electric: [224, 185, 45],
   dragon: [112, 55, 255],
 };
 
@@ -40,9 +45,18 @@ const fetchApi = async (name) => {
 
 search.addEventListener("change", async (event) => {
   const pokemonData = await fetchApi(event.target.value);
+
   // validation pokemon not exist
-  if (!pokemonData) alert("Pokemon does not exist.");
+  if (!pokemonData) {
+    alert("Pokemon does not exist.");
+    return;
+  }
   console.log(pokemonData);
+
+  // main color pokemon
+  const mainColor = typeColors[pokemonData.types[0].type.name];
+  baseStats.style.color = `rgb(${mainColor[0]},${mainColor[1]},${mainColor[2]})`
+  pokedex.style.backgroundColor =`rgb(${mainColor[0]},${mainColor[1]},${mainColor[2]})`
 
   // # of pokemon
   number.innerHTML = "#" + pokemonData.id.toString().padStart(4, 0);
@@ -65,5 +79,17 @@ search.addEventListener("change", async (event) => {
   });
 
   //   base stats
-  
+
+  // console.log(statNumber)
+  pokemonData.stats.forEach((s, i) => {
+    statNumber[i].innerHTML = s.base_stat.toString().padStart(3, "0");
+    // console.log(s.base_stat);
+
+    barInner[i].style.width = `${s.base_stat}%`;
+    // console.log(barInner[i])
+
+    barInner[i].style.backgroundColor = `rgb(${mainColor[0]},${mainColor[1]},${mainColor[2]})`;
+    barOuter[i].style.backgroundColor = `rgba(${mainColor[0]},${mainColor[1]},${mainColor[2]},0.3)`;
+    statDesc[i].style.color           = `rgb(${mainColor[0]},${mainColor[1]},${mainColor[2]})`;
+  });
 });
